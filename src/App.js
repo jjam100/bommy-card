f991fe285261a7e885bba5221c9d8f05f1439659
Hong Jin Baek <hjjam100@gmail.com>
2020-05-01 16:48:36 +0900

    COMP Footer

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import GlobalStyles from "./Components/GlobalStyles";
 
 import Container from "./Components/Container";
 import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <>
       <Container>
         <Header />
+        <Footer />
       </Container>
       <GlobalStyles />
     </>
---
+++ src/Components/Footer.js
@@ -0,0 +1,49 @@
+import React from "react";
+import styled from "styled-components";
+import theme from "./theme";
+
+const FooterWrapper = styled.footer`
+  text-align: center;
+
+  #devider {
+    display: block;
+    width: 100px;
+    height: 3px;
+    border: none;
+    box-shadow: none;
+    background-color: ${theme.color.beige};
+  }
+
+  #copyright {
+    font-family: sans-serif;
+    #instagram-link i {
+      font-weight: 800;
+      font-style: italic;
+
+      &:hover {
+        color: ${theme.color.beige};
+      }
+    }
+  }
+`;
+
+const Footer = (props) => (
+  <FooterWrapper>
+    <hr id="devider" />
+    <p id="copyright">
+      Copyright (c) 2020 <a href="http://www.jjam100.xyz/">JJAM100</a>.
+      <br />
+      😻 with
+      <a
+        href="https://www.instagram.com/cat__bommy/"
+        target="_blank"
+        rel="noopener"
+        id="instagram-link"
+      >
+        <i>@cat_bommy</i>
+      </a>
+    </p>
+  </FooterWrapper>
+);
+
+export default Footer;
