9cd2f774f853e694350a50badb5a7a85256e7677
Hong Jin Baek <hjjam100@gmail.com>
2020-05-01 21:35:34 +0900

    ADD firebase & analytics

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 import React from "react";
 import GlobalStyles from "./Components/GlobalStyles";
 
+import firebase from "./util/fire";
+
 import Container from "./Components/Container";
 import Header from "./Components/Header";
 import Main from "./Components/Main";
 import Footer from "./Components/Footer";
 
 function App() {
+  firebase.analytics();
   return (
     <>
       <Container>
